3c78dfbc634b58a8c27e10e19c08e6c6515fa175
anthonyVargasJesus <104240773+anthonyVargasJesus@users.noreply.github.com>
2025-02-24 14:16:45 -0500

    config para heroku

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ let express = require('express');
 
 let app = express();
 
-app.use(express.static(__dirname + '/dist/pangolin'));
+app.use(express.static(__dirname + '/dist/qualifier'));
 
 app.get('/*', (req, resp) =>{
-    resp.sendFile(__dirname + '/dist/pangolin/index.html');
+    resp.sendFile(__dirname + '/dist/qualifier/index.html');
 });
 
 app.listen(process.env.PORT || 8080);
